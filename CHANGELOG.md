b2f981beb61b3dd23948f62b0aa363842e40c5b1
Gabriel Csapo <gabecsapo@gmail.com>
2017-07-15 02:03:56 -0700

    0.1.0 - show dropdown menu, instead of electron webview     - default sync interval is 60 seconds, should use less CPU

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.1.0 (07/04/2017)
+
+- show dropdown menu, instead of electron webview
+- default sync interval is 60 seconds, should use less CPU
+
 # 0.0.1 (07/04/2017)
 
 - quick and easy working example
---
+++ index.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow, ipcMain, Tray, Menu, MenuItem, shell } = require('electron');
+
+const path = require('path')
+const local = require('local-npm');
+
+const assetsDir = path.join(__dirname, 'assets');
+const tempDir = path.join(__dirname, 'temp');
+
+const server = local({
+    directory: tempDir,
+    port: 5678,
+    pouchPort: 3040,
+    logLevel: 'error',
+    remote: 'https://registry.npmjs.org',
+    remoteSkim: 'https://replicate.npmjs.com',
+    url: 'http://127.0.0.1:5080',
+    syncInterval: 60000
+}, () => {
+    console.log('listening!');
+});
+
+let tray;
+
+app.on('ready', () => {
+  tray = new Tray(path.resolve(__dirname, './assets/cloudTemplate.png'));
+
+  var requests = 0;
+  var sync = 0;
+  function getMenu() {
+    return Menu.buildFromTemplate([
+      {label: 'http://localhost:5678', click: function() { shell.openExternal('http://localhost:5678/_browse') } },
+      {label: `requests: ${requests}`, type: 'normal'},
+      {label: `sync: ${sync}`, type: 'normal'}
+    ]);
+  }
+
+  process.on('request', (args) => {
+    ++requests;
+  })
+
+  process.on('sync', (args) => {
+    sync = args[1];
+  })
+
+  tray.on('click', function() {
+      tray.popUpContextMenu(getMenu())
+  })
+});
